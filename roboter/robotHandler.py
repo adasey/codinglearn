3c33e1b492d67e1ad5c9034264776b2ae95c32e6
Mlink <lmo9903@gmail.com>
2022-03-22 13:00:41 +0900

    robotHandler edit but not finish

--- roboter/robotHandler.py
+++ roboter/robotHandler.py
@@ -2,11 +2,10 @@ import robotReader as robRead
 import robotWriter as robWrite
 import robotData as robData
 
-class HandleRobot(robData.Guest):
+class HandleRobot():
     NAME_OR_COUNT = "Name,Count"
 
     def __init__(self, location):
-
         self.location = location
         self.read = robRead.RobotReader(location)
         self.write = robWrite.RobotWriter()
@@ -28,13 +27,14 @@ class HandleRobot(robData.Guest):
         except:
             print("set empty list check out input value of list")
 
-    
+"""
 m = HandleRobot("d:/Project/Python/codinglearn/fileSys/test.csv")
 k = HandleRobot("d:/Project/Python/codinglearn/test.csv")
 
+a = robData.Guest()
 b = robData.Guest()
 
-text = m.textChange()
+text = k.textChange()
 
 for row in text:
     b.appendGroup(row[0], row[1])
@@ -43,3 +43,4 @@ for a, b in b.Group():
     print(a, b)
 
 #print(b.Group()[0][0]) A print(b.Group()[0]) (A, 3)
+"""

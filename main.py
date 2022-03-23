4e1f66abf91d47f02df824dbe724b4eb8fbc919d
Mlink <lmo9903@gmail.com>
2022-03-23 23:48:52 +0900

    main version 0.11e

--- main.py
+++ main.py
@@ -47,87 +47,88 @@ def isYesOrNo(answer):
         return isYesOrNo(answer)
 
 
-try:
-    while True:
-        people_text = handle_people.textChange()
-        ranking_text = handle_ranking.textChange()
+#try:
+while True:
+    people_text = handle_people.textChange()
+    ranking_text = handle_ranking.textChange()
 
-        user_answer = ""
-        is_yes = False
-        contents = ""
-        template_text = ""
+    user_answer = ""
+    is_yes = False
+    contents = ""
+    template_text = ""
 
-        for row in people_text:
-            people.appendGroup(row[0], row[1])
+    for row in people_text:
+        people.appendGroup(row[0], row[1])
 
-        for row in ranking_text:
-            rest.appendGroup(row[0], row[1])
+    for row in ranking_text:
+        rest.appendGroup(row[0], row[1])
 
-        print(talk.blue(talk.welcome))
-        name = input()
+    print(talk.blue(talk.welcome))
+    name = input()
 
         
-        if not people.isHadName(name):
-            people.appendGroup(name, "1")
+    if not people.isHadName(name):
+        people.appendGroup(name, "1")
+
+    else:
+        template_text = string.Template(talk.welcome_back)
+        contents = template_text.substitute(name = name)
+        print(talk.yellow(contents))
+
+        people.countPlus(name)
+
+        
+    if rest.isRestExist():
+        template_text = string.Template(talk.recommand_script)
+        contents = template_text.substitute(restaurant = rest.countList[0])
+        print(talk.green(contents))
+
+        answer = input()
+
+        if isYesOrNo(answer):
+            rest.countPlus(answer)
 
         else:
-            template_text = string.Template(talk.welcome_back)
-            contents = template_text.substitute(name = name)
-            print(talk.yellow(contents))
-
-            people.countPlus(name)
-
-        
-        if rest.isRestExist():
-            template_text = string.Template(talk.recommand_script)
-            contents = template_text.substitute(restaurant = rest.countList[0])
-            print(talk.green(contents))
-
-            answer = input()
-
-            if isYesOrNo(answer):
+                    
+            if rest.isHadSecond():
+                template_text = string.Template(talk.recommand_script)
+                contents = template_text.substitute(restaurant = rest.countList[1])
+                print(talk.green(contents))
+                    
+                answer = input()
                 rest.countPlus(answer)
 
             else:
+                template_text = string.Template(talk.restaurant_script)
+                contents = template_text.substitute(name = name)
+                print(talk.green(contents))
                     
-                if rest.isHadSecond():
-                    template_text = string.Template(talk.recommand_script)
-                    contents = template_text.substitute(restaurant = rest.countList[1])
-                    print(talk.green(contents))
-                    
-                    answer = input()
-                    rest.countPlus(answer)
+                restaurant = input()
+                rest.appendGroup(restaurant, "1")
 
-                else:
-                    template_text = string.Template(talk.restaurant_script)
-                    contents = template_text.substitute(name = name)
-                    print(talk.green(contents))
-                    
-                    restaurant = input()
-                    rest.appendGroup(restaurant, "1")
-
-        else:
-            template_text = string.Template(talk.restaurant_script)
-            contents = template_text.substitute(name = name)
-            print(talk.green(contents))
-            
-            restaurant = input()
-            rest.appendGroup(restaurant, "1")
-
-
-        handle_people.saveFile(people.Group())
-        rest.setGroupSorted()
-        handle_ranking.saveFile(rest.Group())
-
-        template_text = string.Template(talk.end)
+    else:
+        template_text = string.Template(talk.restaurant_script)
         contents = template_text.substitute(name = name)
-        print(talk.blue(contents))
-        
-        break
+        print(talk.green(contents))
+            
+        restaurant = input()
+        rest.appendGroup(restaurant, "1")
 
+    
+    handle_people.saveFile(people.Group())
+    rest.setGroupSorted()
+    handle_ranking.saveFile(rest.Group())
+
+    template_text = string.Template(talk.end)
+    contents = template_text.substitute(name = name)
+    print(talk.blue(contents))
+        
+    break
+"""
 except Exception as error:
     write_people.InitFileWriter(write_people.PEOPLE_FILE_LOCATION)
     write_rest.InitFileWriter(write_rest.RANKING_FILE_LOCATION)
 
     print(error.with_traceback)
     print(error)
+"""
